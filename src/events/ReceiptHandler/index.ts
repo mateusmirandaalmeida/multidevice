1fa3ca1a728dafd94683e5e5badac53f58e51497
Tarikh Agustia <agustia.tarikh150@gmail.com>
2021-09-30 06:46:55 +0700

    Adding Receipt ACK

--- src/events/ReceiptHandler/index.ts
+++ src/events/ReceiptHandler/index.ts
@@ -1,10 +1,22 @@
+import { encodeStanza } from '../../proto/Stanza';
 import { WapNode } from '../../proto/WapNode';
 import { Handler } from '../Handler';
 
 export class ReceiptHandler extends Handler {
   public async handle(node: WapNode) {
     const data = await this.parse(node);
+    const receipt = new WapNode(
+      'ack',
+      {
+        class: 'receipt',
+        id: data.id,
+        to: data.from,
+        type: data.type
+      },
+      null,
+    );
 
+    this.socket.sendFrame(encodeStanza(receipt));
     this.client.emit('message-status-updated', data);
     return true;
   }
@@ -20,7 +32,9 @@ export class ReceiptHandler extends Handler {
   public async parse(stanza: WapNode) {
     return {
       id: stanza.attrs.id,
-      status: stanza.attrs.type ? stanza.attrs.type : 'deliveried'
+      status: stanza.attrs.type ? stanza.attrs.type : 'deliveried',
+      type: stanza.attrs.type,
+      from: stanza.attrs.from
     };
   }
 }
